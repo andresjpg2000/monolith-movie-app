bea510bb4dbe5a979c9659a15c6b11a90ac46709
andresjpg2000 <andresjpg2000@gmail.com>
2025-11-05 17:14:13 +0000

    refactor: remove redundant log level configuration in logger setup

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const pino = require("pino");
-const logger = pino({ level: process.env.LOG_LEVEL || "info" }, { transport : { target: 'pino-pretty', options: { colorize: true } } });
+const logger = pino({ transport : { target: 'pino-pretty', options: { colorize: true } } });
 
 // our "database" data
 let users = [
